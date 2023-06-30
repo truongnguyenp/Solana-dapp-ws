fd2503cacf8130be8dde6dc9a06e9597ca7f2c9c
truongnguyenp <truongnguyen310.work@gmail.com>
2023-06-30 20:33:22 +0700

    feat: add wallet adapter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,40 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { WalletContextProvider } from '@/contexts/WalletContextProvider';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { useMemo } from 'react';
+import { clusterApiUrl } from '@solana/web3.js'
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  SolletWalletAdapter,
+  SolletExtensionWalletAdapter,
+  TorusWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+
 export default function App({ Component, pageProps }: AppProps) {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new SolletWalletAdapter({ network }),
+      new SolletExtensionWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <WalletContextProvider
+      endpoint={endpoint}
+      network={network}
+      wallets={wallets}
+    >
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </WalletContextProvider>
   );
 }
