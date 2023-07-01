f257d3a6a404d726aed2a8145935989e4b7dde1b
minh bear <minhbeardev@gmail.com>
2023-07-01 10:15:16 +0700

    feat: add UI for send feature

---
+++ src/components/ElusivApp.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react'
+import { Elusiv, SEED_MESSAGE, TopupTxData } from '@elusiv/sdk'
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { useToggle } from 'usehooks-ts';
+import Modal from './common/Modal';
+import { Button } from '@chakra-ui/react';
+
+export default function ElusivApp() {
+    const { publicKey, signMessage, signTransaction } = useWallet()
+    const [elusiv, setElusiv] = useState<Elusiv>()
+    const { connection } = useConnection()
+
+    useEffect(() => {
+        const getElusiv = async () => {
+            if (publicKey && signMessage) {
+                const encodedMessage = new TextEncoder().encode(SEED_MESSAGE)
+
+                const seed = await signMessage(encodedMessage)
+                const elusivInstance = await Elusiv.getElusivInstance(seed, publicKey, connection, 'devnet')
+                setElusiv(elusivInstance);
+            }
+        }
+
+        getElusiv()
+
+        return () => {
+            setElusiv(undefined)
+        }
+
+    }, [publicKey, connection])
+
+    const [isTopUpModalVisible, toggleTopUpModalVisible, setIsTopUpModal] = useToggle();
+    const [isSendModalVisible, toggleSendModalVisible, setIsSendModa] = useToggle();
+    const [isViewTransactionModalVisible, toggleViewTransactionModalVisible, setIsViewTransactionModa] = useToggle();
+
+
+    return (
+        <div>
+            <Button onClick={toggleTopUpModalVisible}>
+                    Topup
+            </Button>
+            <Button onClick={toggleTopUpModalVisible}>
+                    Topup
+            </Button>
+           {
+            <Modal
+            isOpen={isTopUpModalVisible}
+            onClose={()=>{
+                toggleTopUpModalVisible();
+            }}
+            />
+
+           }
+        </div>
+    )
+}
---
+++ src/components/common/Modal.tsx
@@ -0,0 +1,43 @@
+import {
+    Modal as ChakraModal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+  } from '@chakra-ui/react';
+import React from 'react';
+
+export default function Modal({
+    onClose,
+    isOpen 
+}: {
+    onClose: ()=>void,
+    isOpen: boolean
+}
+){
+    return(
+        <ChakraModal
+        isCentered
+        onClose={onClose}
+        isOpen={isOpen}
+        motionPreset='slideInBottom'
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </ChakraModal>
+    )
+}
